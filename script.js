8ab01f3054f2db22808868e0af11dcaf5502fc94
erdoganakpolat <erdoganakpolat98@gmail.com>
2024-11-23 18:22:27 +0300

    commit 2

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
-"use strict";
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
+'use strict';
 
-Car.prototype.accelerate = function () {
-  this.speed += 10;
-  console.log(`${this.make} is going at ${this.speed}`);
-};
+// PROJECT -- 1 --
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
 
-Car.prototype.brake = function () {
-  this.speed -= 5;
-  console.log(`${this.make} is going at ${this.speed}`);
-};
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(`${this.make} is going at ${this.speed}`);
+// };
 
-const bmw = new Car("BMW", 120);
-const mercedes = new Car("Mercedes", 95);
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+//   console.log(`${this.make} is going at ${this.speed}`);
+// };
 
-bmw.accelerate();
-bmw.accelerate();
-bmw.brake();
+// const bmw = new Car('BMW', 120);
+// const mercedes = new Car('Mercedes', 95);
+
+// bmw.accelerate();
+// bmw.accelerate();
+// bmw.brake();
+
+// ------------------------------------------------------------------
+// class expression
+// const PersonCl = class {}
+
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.birthYear = firstName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(2045 - this.birthYear);
+  }
+}
+const erdogan = new PersonCl('Erdogan', 1998);
+console.log(erdogan);
+erdogan.calcAge();
+
+console.log(erdogan.__proto__ === PersonCl.prototype);
